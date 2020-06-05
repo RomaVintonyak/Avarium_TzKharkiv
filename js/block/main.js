f4c316a955e18a735daf8b2436151fe57832eec4
RomaVintonyak <19roman23@gmail.com>
2020-06-05 22:46:29 +0300

    create modal window

--- js/block/main.js
+++ js/block/main.js
@@ -1,4 +1,27 @@
 jQuery(document).ready(function(){
   "use script";
-  
+  //show modal window
+  var showModal = $("#quote");
+  var modalWindow = $("#modalQuote");
+  showModal.on("click", function(){
+    modalWindow.addClass("modal__window--show");
+    $("body").css({
+      "overflow-y": "hidden"
+    });
+  });
+  //close modal window
+  var closeModal = $("#modalClose");
+  var modalOverlay = $("#modalQuote");
+  closeModal.on("click", function(){
+    modalWindow.removeClass("modal__window--show");
+    $("body").css({
+      "overflow-y": "scroll"
+    });
+  });
+  modalOverlay.on("click", function(){
+    modalWindow.removeClass("modal__window--show");
+    $("body").css({
+      "overflow-y": "scroll"
+    });
+  });
 });
